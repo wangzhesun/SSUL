7dd5f78471c993e84cc8f006668a46809c8477b6
wangzhesun <wangzhe.sun@outlook.com>
2022-08-24 10:50:15 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -45,7 +45,8 @@ python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 -
                 --pseudo_thresh ${THRESH} --freeze --bn_freeze --unknown --w_transfer --amp \
                 --mem_size ${MEMORY} \
                 --few_shot ${FEWSHOT} --num_shot ${NUMSHOT} --start_step ${START} \
-                --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split2_step_0_disjoint.pth \
+                --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split3_step_0_disjoint.pth \
+#                --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split2_step_0_disjoint.pth \
 
 
 # non few shot
