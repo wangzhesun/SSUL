bec7ad3523372b170e0353758d9e1fca27d24d92
wangzhesun <wangzhe.sun@outlook.com>
2022-09-13 22:22:44 -0500

    Update run_coco.sh

--- run_coco.sh
+++ run_coco.sh
@@ -18,7 +18,7 @@ python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 -
                 --dataset ${DATASET} --task ${TASK} --lr_policy poly --pseudo \
                 --pseudo_thresh ${THRESH} --freeze --bn_freeze --unknown --w_transfer --amp \
                 --mem_size ${MEMORY} \
-                --few_shot ${FEWSHOT} --num_shot ${NUMSHOT} --start_step ${START} --test_only True
+                --few_shot ${FEWSHOT} --num_shot ${NUMSHOT} --start_step ${START} --test_only
 
 ##### few shot step 1 - last
 #python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 --crop_val \
