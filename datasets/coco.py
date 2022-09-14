a3dbe0baba8800f23298eb5005ae1f70c7306235
wangzhesun <wangzhe.sun@outlook.com>
2022-09-13 19:59:51 -0500

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -315,6 +315,7 @@ class COCOSegmentation(data.Dataset):
         img, target, file_name = self.dataset[index]
 
         #########################################
+        print(type(img))
         print(type(target))
         print(target.size())
         #########################################
