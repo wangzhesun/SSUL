e0361389961b5a6650289b6e42042b07ebd0a1e9
wangzhesun <wangzhe.sun@outlook.com>
2022-09-13 22:19:22 -0500

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -32,14 +32,14 @@ class BCEWithLogitsLossWithIgnoreIndex(nn.Module):
         n_cl = torch.tensor(inputs.shape[1]).to(inputs.device)
         labels_new = torch.where(targets != self.ignore_index, targets, n_cl)
 
-        print('1')
-        print(inputs.shape[1] + 1)
+        # print('1')
+        # print(inputs.shape[1] + 1)
 
         # replace ignore with numclasses + 1 (to enable one hot and then remove it)
         targets = F.one_hot(labels_new, inputs.shape[1] + 1).float().permute(0, 3, 1, 2)
 
         ###########################################################################
-        print('2')
+        # print('2')
         ###########################################################################
 
 
