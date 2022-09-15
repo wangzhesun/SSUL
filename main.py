79d1b52a44dc7b38891915d0df3bc2ab04410381
wangzhesun <wangzhe.sun@outlook.com>
2022-09-15 00:41:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1005,14 +1005,14 @@ def main(opts, seed):
     # print(torch.max(dataset_dict['train'][7][0]))
     # print(torch.min(dataset_dict['train'][7][0]))
     # print(dataset_dict['train'][7][0].is_cuda)
-    print('\n printing max and min index 7777 train label: ')
-    print(dataset_dict['train'][7777][1].size())
-    print(torch.max(dataset_dict['train'][7777][1]))
-    print(torch.min(dataset_dict['train'][7777][1]))
-    print(dataset_dict['train'][7777][1].is_cuda)
-
-    print('\n printing unique class in train label: ')
-    print(torch.unique(dataset_dict['train'][7777][1]))
+    # print('\n printing max and min index 7777 train label: ')
+    # print(dataset_dict['train'][7777][1].size())
+    # print(torch.max(dataset_dict['train'][7777][1]))
+    # print(torch.min(dataset_dict['train'][7777][1]))
+    # print(dataset_dict['train'][7777][1].is_cuda)
+    #
+    # print('\n printing unique class in train label: ')
+    # print(torch.unique(dataset_dict['train'][7777][1]))
     # return
     ##################################################################################
 
