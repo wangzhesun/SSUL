3203ca6eed2c52bd213de934e4a6bd15b95e1f80
wangzhesun <wangzhe.sun@outlook.com>
2022-09-14 23:55:36 -0500

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -331,13 +331,13 @@ class COCOSegmentation(data.Dataset):
         ################################################################
 
         # add unknown label, background index: 0 -> 1, unknown index: 0
-        # if self.image_set == 'train' and self.unknown:
-        #     target = torch.where(target == 255,
-        #                          torch.zeros_like(target) + 255,  # keep 255 (uint8)
-        #                          target + 1)  # unknown label
-        #
-        #     unknown_area = (target == 1)
-        #     target = torch.where(unknown_area, torch.zeros_like(target), target)
+        if self.image_set == 'train' and self.unknown:
+            target = torch.where(target == 255,
+                                 torch.zeros_like(target) + 255,  # keep 255 (uint8)
+                                 target + 1)  # unknown label
+
+            unknown_area = (target == 1)
+            target = torch.where(unknown_area, torch.zeros_like(target), target)
 
         return img, target.long(), sal_map, file_name
         # return self.dataset[index]
