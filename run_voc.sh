6c7cae5d6c31a564bf8a1b4bf602285c13d95e22
wangzhesun <wangzhe.sun@outlook.com>
2022-09-28 19:33:35 -0500

    Update run_voc.sh

--- run_voc.sh
+++ run_voc.sh
@@ -18,7 +18,7 @@
 #DATA_ROOT=../data/COCO2017
 DATA_ROOT=../data/VOCdevkit/VOC2012
 DATASET=voc
-TASK=15-1-split3 # [15-1, 10-1, 19-1, 15-5, 5-3, 5-1, 2-1, 2-2] add split to task 15-1
+TASK=15-1-split0 # [15-1, 10-1, 19-1, 15-5, 5-3, 5-1, 2-1, 2-2] add split to task 15-1
 EPOCH=50 #50
 BATCH=26 #32
 LOSS=bce_loss
@@ -27,7 +27,7 @@ THRESH=0.7
 FEWSHOT=True
 #NUMSHOT=5
 #NUMSHOT=1
-NUMSHOT=150
+NUMSHOT=40
 START=1
 #START=0
 MEMORY=500 #100 # [0 (for SSUL), 100 (for SSUL-M)]
@@ -47,7 +47,7 @@ python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 -
                 --pseudo_thresh ${THRESH} --freeze --bn_freeze --unknown --w_transfer --amp \
                 --mem_size ${MEMORY} \
                 --few_shot ${FEWSHOT} --num_shot ${NUMSHOT} --start_step ${START} \
-                --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split3_step_0_disjoint.pth \
+                --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split0_step_0_disjoint.pth \
 
 
 ##### non few shot
