dd1c093b84bf4dbbcf499749646cf75cf261ad1c
wangzhesun <wangzhe.sun@outlook.com>
2022-11-06 23:29:37 -0600

    Update run_ade.sh

--- run_ade.sh
+++ run_ade.sh
@@ -2,10 +2,10 @@
 DATA_ROOT=../data/ADEChallengeData2016
 DATASET=ade
 TASK=100-5 # [15-1, 10-1, 19-1, 15-5, 5-3, 5-1, 2-1, 2-2] add split to task 15-1
-EPOCH=50 #50
+EPOCH=100 #50
 BATCH=16 #32
 LOSS=bce_loss
-LR=0.01
+LR=0.05
 THRESH=0.7
 FEWSHOT=True
 #NUMSHOT=5
@@ -13,7 +13,7 @@ FEWSHOT=True
 NUMSHOT=40
 #START=1
 START=0
-MEMORY=500 #100 # [0 (for SSUL), 100 (for SSUL-M)]
+MEMORY=300 #100 # [0 (for SSUL), 100 (for SSUL-M)]
 
 ##### few shot step 0
 python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 --crop_val \
