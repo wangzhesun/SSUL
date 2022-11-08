3d4b94d337becf00154bdb6d457ef9b74a1f6e4f
wangzhesun <wangzhe.sun@outlook.com>
2022-11-07 22:06:07 -0600

    Update run_ade.sh

--- run_ade.sh
+++ run_ade.sh
@@ -1,7 +1,7 @@
 #DATA_ROOT=../data/COCO2017
 DATA_ROOT=../data/ADEChallengeData2016
 DATASET=ade
-TASK=100-50 # [15-1, 10-1, 19-1, 15-5, 5-3, 5-1, 2-1, 2-2] add split to task 15-1
+TASK=100-5 # [15-1, 10-1, 19-1, 15-5, 5-3, 5-1, 2-1, 2-2] add split to task 15-1
 EPOCH=100 #50
 BATCH=14 #32
 LOSS=bce_loss
